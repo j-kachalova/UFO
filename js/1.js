d7547521a7ee9e70a95784aea496664e52c26b9a
j-kachalova <j-kachalova@yandex.ru>
2020-06-09 23:22:49 +0400

    сделала сервис с одним вопросом в новостях
    нужно добавить несколько вопросов на разных страницах и сброс настроек

---
+++ js/1.js
@@ -0,0 +1,58 @@
+function NewsCard(title, kind, poster, country = "Россия") {
+  this.title = title,
+    this.kind = kind,
+    this.poster = poster,
+    this.country = country
+}
+let news = [
+  new NewsCard("США ПЫТАЕТСЯ СКРЫТЬ ВСЮ ПРАВДУ! НО ТЕПЕРЬ ВЕСЬ МИР ЗНАЕТ ПРИЧИНУ ПОЯВЛЕНИЯ COVID-19!", "ufo",'url(https://cdn.pixabay.com/photo/2016/11/12/15/29/science-fiction-1819026_1280.jpg)', country = "США"),
+  new NewsCard("УЧЕНЫЕ В ШОКЕ ОТ НОВОЙ НАХОДКИ", "ufo", "url(https://cdn.pixabay.com/photo/2018/07/14/11/33/network-3537401_1280.jpg)", "ЮАР"),
+  new NewsCard("НЕОБЫЧНЫЙ ЛЕТАЮЩИЙ ОБЪЕКТ БЫЛ ЗАМЕЧЕН В НОЧНОМ НЕБЕ ДЕВЯТОГО УДЕЛА", "ufo", "url(https://img.gazeta.ru/files3/412/11497412/pentagon-80394_1920-pic4_zoom-1500x1500-99448.jpg)", "Россия"),
+  new NewsCard("МЫ БЫЛИ В ШОКЕ, КОГДА НА ПУБЛИКЕ ПОЯВИЛСЯ ОН....", "ufo", "url(https://cdn-st1.rtr-vesti.ru/p/o_1554044.jpg)", "Не только Россия")
+];
+
+document.getElementById('test').addEventListener("submit", function (event) {
+  event.preventDefault(); //Отменяет действия по умолчанию для данного события
+  let q1Inputs = document.getElementsByName('q1');
+  let checkedValue = '';
+  q1Inputs.forEach(function (input) {
+    if (input.checked) checkedValue = input.value;
+  });
+  localStorage.setItem('q1', checkedValue);
+
+  getNewsCard();
+});
+
+let getNewsCard = function () {
+  let result = [];
+  if (localStorage.getItem('q1') == 'Россия') {
+    result = news.filter(n => n.country == 'Россия');
+  } else {
+    result = news.filter(n => n.country != 'Россия');
+  }
+
+  let container = document.getElementById('news-cards');
+  container.innerHTML = '';
+  result.forEach(function (cardNews) {
+    console.log('lhfkghc')
+    let text = document.createElement('p');
+    text.classList.add('news__p');
+    text.innerText = cardNews.title;
+    let blockText = document.createElement('div');
+    blockText.classList.add('news__text');
+    blockText.appendChild(text);
+    let img = document.createElement('div');
+    img.classList.add('news__img');
+    img.style.backgroundImage = cardNews.poster;
+    let blockImage = document.createElement('div');
+    blockImage.classList.add('news__block');
+    blockImage.appendChild(img);
+    blockImage.appendChild(blockText);
+    let card = document.createElement('a');
+    card.classList.add('news-link');
+    card.appendChild(blockImage);
+    container.appendChild(card);
+  });
+}
+
+getNewsCard();
