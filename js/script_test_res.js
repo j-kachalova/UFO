e7e762bf3df03d2c8210829cee6804f7ed86b33a
FriDay1401 <65306881+FriDay1401@users.noreply.github.com>
2020-06-27 12:32:55 +0400

    Тест 1

--- js/script_test_res.js
+++ js/script_test_res.js
@@ -10,7 +10,7 @@ function resultTest(lastA){
   let test1 = localStorage.getItem('test1-q1');
   let test2 = localStorage.getItem('test1-q2');
   let test3 = localStorage.getItem('test1-q3');
-  if(test1=='ответ1' && test2 == 'ответ2'&& test3 == 'ответ3'){
+  if(test1=='ответ1' && test2 == 'ответ1'&& test3 == 'ответ1'){
       document.location.href='test_result1.html';
   }
   else document.location.href='test_result2.html';
